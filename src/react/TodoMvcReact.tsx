5cf48dfa80086379b54baf6bf4ed0936a5627b40
Taehee Kim <rabolution@gmail.com>
2022-08-19 15:44:30 +0900

    TodoItem 분리

--- src/react/TodoMvcReact.tsx
+++ src/react/TodoMvcReact.tsx
@@ -1,13 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-function TodoMvcReact(){
-    const todoList = ["투두 상태 구현", "마크업", "어쩌구"];
-    return <>
-    <h1>Todo MVC</h1>
-    <ul>
-        {todoList.map((todo) => <li key={todo}>{todo}</li>)}
-    </ul>
+type TodoT = string;
+
+interface TodoItemProps {
+    todo: TodoT;
+}
+
+function TodoItem({ todo }: TodoItemProps) {
+  return <li key={todo}>{todo}</li>;
+}
+
+function TodoMvcReact() {
+  const [todoList, setTodoList] = useState(["투두 상태 구현", "마크업", "어쩌구"]);
+  return (
+    <>
+      <h1>Todo MVC</h1>
+      <ul>
+        {todoList.map((todo) => (
+          <TodoItem todo={todo} />
+        ))}
+      </ul>
     </>
+  );
 }
 
 export default TodoMvcReact;
