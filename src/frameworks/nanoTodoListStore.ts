65ce60467c447778a5b785b53d87266ddc57bc18
Taehee Kim <rabolution@gmail.com>
2022-08-23 19:16:28 +0900

    chore: format nanoTodoListStore

--- src/frameworks/nanoTodoListStore.ts
+++ src/frameworks/nanoTodoListStore.ts
@@ -1,18 +1,24 @@
-import { atom, action, WritableAtom } from 'nanostores';
-import { TodoT } from '../global';
-import { generateId } from './generateId';
+import { atom, action, WritableAtom } from "nanostores";
+import { TodoT } from "../global";
+import { generateId } from "./generateId";
 
-type OmitFirstArg<F> = F extends (x: any, ...args: infer P) => infer R ? (...args: P) => R : never;
+export const todoListStore = atom<TodoT[]>([]);
 
-export const todoListStore = atom<TodoT[]>([])
-
-function createActions<T, R extends Record<string, (old: T, ...args: any) => T>>(store: WritableAtom<T>, rawActions: R){
-  return Object.fromEntries(Object.entries(rawActions).map(([name, rawAction]) => {
-    return [name, action(store, name, (store, ...args) => {
-      const old = store.get();
-      store.set(rawAction(old, ...args))
-    })]
-  }))
+function createActions<
+  T,
+  R extends Record<string, (old: T, ...args: any) => T>
+>(store: WritableAtom<T>, rawActions: R) {
+  return Object.fromEntries(
+    Object.entries(rawActions).map(([name, rawAction]) => {
+      return [
+        name,
+        action(store, name, (store, ...args) => {
+          const old = store.get();
+          store.set(rawAction(old, ...args));
+        }),
+      ];
+    })
+  );
 }
 
 export const actions = createActions(todoListStore, {
@@ -21,29 +27,29 @@ export const actions = createActions(todoListStore, {
       id: generateId(),
       content,
       isCompleted: false,
-    }
-    return [...old, newTodo]
+    };
+    return [...old, newTodo];
   },
   completeTodo(old, id: number, isCompleted: boolean) {
-    const todo = old.find((todo) => todo.id === id)
-    if(todo){
-      todo.isCompleted = isCompleted
+    const todo = old.find((todo) => todo.id === id);
+    if (todo) {
+      todo.isCompleted = isCompleted;
     }
     return [...old];
   },
   changeTodo(old, id: number, newContent: string) {
-    const todo = old.find((todo) => todo.id === id)
-    if(todo){
-      todo.content = newContent
+    const todo = old.find((todo) => todo.id === id);
+    if (todo) {
+      todo.content = newContent;
     }
     return [...old];
   },
   deleteTodo(old, id: number) {
-    return old.filter((todo) => todo.id !== id)
-  }
+    return old.filter((todo) => todo.id !== id);
+  },
 }) as {
   addTodo(content: string): void;
   completeTodo(id: number, isCompleted: boolean): void;
   changeTodo(id: number, newContent: string): void;
   deleteTodo(id: number): void;
-}
+};
