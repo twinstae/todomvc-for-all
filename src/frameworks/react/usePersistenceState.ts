4d0d57dec7c9be23266b9b276e1396aa5ecfa0f7
Taehee Kim <rabolution@gmail.com>
2022-08-23 16:19:08 +0900

    feat: usePersistenceState에 로깅 추가

--- src/frameworks/react/usePersistenceState.ts
+++ src/frameworks/react/usePersistenceState.ts
@@ -1,14 +1,20 @@
 import { useState } from "react";
 import { inject } from "../../dependency";
 
-function usePersistState<T>(initValue: T, storageKey: string) {
+type Options = {
+  log: boolean
+}
+
+function usePersistState<T>(initValue: T, storageKey: string, options: Options = { log: false }) {
   const stoage = inject('storage');
   const [state, setState] = useState<T>(stoage.get(storageKey) || initValue);
 
   return [state, (update: (old: T) => T) => {
     setState(old => {
-      stoage.set(storageKey, update(state));
-      return update(old);
+      const result = update(old);
+      stoage.set(storageKey, result);
+      if (options.log){ console.log(result) }
+      return result;
     });
   }] as const;
 }
